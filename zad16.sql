c85a49d75fff19d71835d971c07b2400b9a0d2fb
Magdalena Kortas <magdalenekortas@gmail.com>
2018-02-18 12:32:01 +0100

    zad 16

---
+++ zad16.sql
@@ -0,0 +1,10 @@
+/* Wymagane statystyki dla każdego operatora:
+ilość wykonanych tras
+ilośc pasażerów
+% tras na czas (w miejscu docelowym)
+% tras anulowanych
+% tras opóźnionych >3h
+% tras uprawnionych do rekompensaty
+suma pasażerów uprawnionych do rekompensaty
+całkowita wartość rekompensaty
+Wymagana możliwość filtrowania po czasie trasy */
