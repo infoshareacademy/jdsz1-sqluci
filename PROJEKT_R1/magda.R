20b449841d1e105f0c5e8aa7ccc5a61cb063d13f
Magdalena Kortas <magdalenekortas@gmail.com>
2018-03-24 10:26:10 +0100

    Rplot partii

--- PROJEKT_R1/magda.R
+++ PROJEKT_R1/magda.R
@@ -29,3 +29,14 @@ ggplot(data = df2) +
     y = results_SLD,
     linetype = research_center,
     color=research_center)) 
+
+ggplot(data = df2) +
+  geom_point(mapping = aes(
+    x = time,
+    y = df2$WOLNOŚĆ,
+    color=research_center)) +
+  geom_smooth(mapping = aes(
+    x = time,
+    y = df2$WOLNOŚĆ,
+    linetype = research_center,
+    color=research_center)) 
