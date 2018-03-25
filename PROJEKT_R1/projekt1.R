59e56a2d7676ff4bd13058fb29b9d75ba7a3a573
Wojciech Artichowicz <miseryslave@o2.pl>
2018-03-25 11:20:14 +0200

    common beginning

--- PROJEKT_R1/projekt1.R
+++ PROJEKT_R1/projekt1.R
@@ -5,23 +5,15 @@ library(tidyr)
 
 link = "https://docs.google.com/spreadsheets/d/1P9PG5mcbaIeuO9v_VE5pv6U4T2zyiRiFK_r8jVksTyk/htmlembed?single=true&gid=0&range=a10:o400&widget=false&chrome=false"
 xData <- getURL(link)
-raw_data <- readHTMLTable(xData,header = TRUE,  stringsAsFactors = FALSE,encoding = "utf8")
-
-tab <- raw_data[[1]]
-
-#as.numeric(gsub(",",".","12,34"))
-
-hdr_names <- c("lp","Osrodek","Zleceniodawca","Publikacja","Metoda badania",
-               "uwzgl niezdecyd.","termin badania","PiS",
-               "PO","K15","SLD",".N","PSL","Partia razem","Wolnosc","N/Z")
-
-tab<- tab[4:236,]
-colnames(tab)<-hdr_names
+raw_data <- readHTMLTable(xData,header = TRUE, skip.rows = c(1,3), stringsAsFactors = FALSE,encoding = "utf8")
+df_dane <- raw_data[[1]]
+head(df_dane)
 
 for (i in 8:16)
-  tab[[i]] <- as.numeric(gsub(",",".",tab[[i]]))
+  df_dane[[i]] <- as.numeric(gsub(",",".",df_dane[[i]]))
   
 
+#wojtek
 ggplot(data = tab) + 
   geom_point(mapping = aes(
     x = as.Date(tab$Publikacja,"%d.%m.%y"),
