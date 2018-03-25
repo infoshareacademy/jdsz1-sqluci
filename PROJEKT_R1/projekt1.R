b2022052ac81ce3d6a3ef53a2fe8fa73cf03bb53
Wojciech Artichowicz <miseryslave@o2.pl>
2018-03-25 11:24:38 +0200

    common beginning

--- PROJEKT_R1/projekt1.R
+++ PROJEKT_R1/projekt1.R
@@ -3,15 +3,16 @@ library(RCurl)
 library(tidyr)
 #install.packages("tidyr")
 
-link = "https://docs.google.com/spreadsheets/d/1P9PG5mcbaIeuO9v_VE5pv6U4T2zyiRiFK_r8jVksTyk/htmlembed?single=true&gid=0&range=a10:o400&widget=false&chrome=false"
-xData <- getURL(link)
-raw_data <- readHTMLTable(xData,header = TRUE, skip.rows = c(1,3), stringsAsFactors = FALSE,encoding = "utf8")
-df_dane <- raw_data[[1]]
-head(df_dane)
-
+link <- "https://docs.google.com/spreadsheets/d/1P9PG5mcbaIeuO9v_VE5pv6U4T2zyiRiFK_r8jVksTyk/htmlembed?single=true&gid=0&range=a10:o400&widget=false&chrome=false" 
+xData <- getURL(link)  #get link
+dane_z_html <- readHTMLTable(xData, stringsAsFactors = FALSE, skip.rows = c(1,3), encoding = "utf8") #read html
+df_dane <- as.data.frame(dane_z_html)   #data frame
+colnames(df_dane) <- df_dane[1,]  #nazwy kolumn
+df2 <- df_dane[2:nrow(df_dane),]  #pominięcie pierwszego wiersza
 for (i in 8:16)
-  df_dane[[i]] <- as.numeric(gsub(",",".",df_dane[[i]]))
-  
+  df2[[i]] <- as.numeric(gsub(",",".",df2[[i]]))      #przecinki
+df2
+
 
 #wojtek
 ggplot(data = tab) + 
