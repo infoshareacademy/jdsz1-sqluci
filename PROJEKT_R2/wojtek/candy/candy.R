012bceb1a157326bce970f1b734dafbd2c93acee
wojciechartichowicz <wojciech.artichowicz@gmail.com>
2018-05-28 22:54:43 +0200

    log reg

--- PROJEKT_R2/wojtek/candy/candy.R
+++ PROJEKT_R2/wojtek/candy/candy.R
@@ -15,9 +15,9 @@ summary(cdf)
 mapply(anyNA, cdf) #nope! go further
 
 #visualizing data
-ggplot(cdf) + geom_point(aes(x = sugarpercent,y=pricepercent)) # no dependence
-ggplot(cdf) + geom_point(aes(x = sugarpercent,y= winpercent)) # no dependence
-ggplot(cdf) + geom_point(aes(x = pricepercent,y= winpercent)) # no dependence
+ggplot(cdf) + geom_point(aes(x = sugarpercent,y=pricepercent,col = winpercent)) # no dependence
+ggplot(cdf) + geom_point(aes(x = sugarpercent,y= winpercent, col = pricepercent)) # no dependence
+ggplot(cdf) + geom_point(aes(x = pricepercent,y= winpercent, col = sugarpercent)) # no dependence
 
 pca_real = FactoMineR::PCA(cdf[c("sugarpercent","pricepercent","winpercent")]) #cannot be linearly separated
 print(pca_real$eig)
@@ -41,3 +41,52 @@ for (i in 2:8)
 
 
 ggplot(cdf[c(1,2)]) + geom_bar(aes(x = chocolate))
+
+ggplot(cdf[c(1,2)]) + geom_bar(aes(x = chocolate))
+
+N = nrow(cdf)
+scrambled_index <- sample(1:N)
+
+edge <- round(0.8 *N)
+train <- scrambled_index[1:edge]
+test <- scrambled_index[(edge+1):N]
+feat <- c("fruity", "caramel", "peanutyalmondy", "nougat", "crispedricewafer", "hard", "bar", "pluribus", "sugarpercent", "pricepercent", "winpercent")
+target <- "chocolate"
+
+train.df <- cdf[train,c(feat,target)]
+model <- glm(chocolate ~.,family=binomial(link='logit'),data=train.df)
+
+summary(model)
+anova(model, test="Chisq")
+
+
+test.df <- cdf[test,c(feat,target)]
+pred_choco <- ifelse(predict(model,newdata=test.df,type='response') > 0.5,1,0)
+
+check <- data.frame(predicted = pred_choco,observed = test.df$chocolate)
+
+confusion.matrix <- table(check$predicted,check$observed)
+
+predicted <- factor(c(0, 0, 1, 1))
+observed <- factor(c(0, 1, 0, 1))
+Y      <- as.vector( confusion.matrix)
+confusion.matrix.df <- data.frame(predicted, observed, Y)
+
+ggplot(data =  confusion.matrix.df, mapping = aes(x = observed, y = predicted)) +
+  geom_tile(aes(fill = Y), colour = "white") +
+  geom_text(aes(label = sprintf("%1.0f",Y)), vjust = 1) +
+  scale_fill_gradient(low = "white", high = "steelblue")
+
+chisq_var <- c("chocolate","fruity","bar","winpercent")
+
+
+ROCRpred <- prediction(pred_choco, test.df$chocolate)
+ROCRperf <- performance(ROCRpred, 'tpr','fpr')
+plot(ROCRperf, colorize = TRUE)
+
+
+pca_train <- FactoMineR::PCA(train.df)
+eigv <- pca_all$ind$coord
+pt <- as.numeric(cdf[1,feat])
+matrix(pt,11,1)%*%eigv
+
